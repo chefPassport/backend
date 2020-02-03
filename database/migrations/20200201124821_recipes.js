cba0969bcea74da5ac64dbd019f49dfe39821f5c
Vanessa <vanessatellez45@gmail.com>
2020-02-03 11:53:35 -0500

    took out meal type table

--- database/migrations/20200201124821_recipes.js
+++ database/migrations/20200201124821_recipes.js
@@ -8,12 +8,7 @@ exports.up = function(knex) {
     recipes.string('image').notNullable();
     recipes.text('ingredients').notNullable();
     recipes.text('instructions').notNullable();
-    recipes.integer('meal_type_id').notNullable()
-        .unsigned()
-        .references('id')
-        .inTable('meal_type')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
+    recipes.string('meal_type').notNullable();
     recipes.integer('chef_id').notNullable()
         .unsigned()
         .references('id')
@@ -21,14 +16,9 @@ exports.up = function(knex) {
         .onDelete('CASCADE')
         .onUpdate('CASCADE');
   })
-  .createTable('meal_type', mealtypes => {
-    mealtypes.increments();
-    mealtypes.string('meal_type_name', 128).notNullable();
-    });
 };
 
 exports.down = function(knex) {
   return knex.schema
-  .dropTableIfExists('meal_type')
   .dropTableIfExists('recipes');
 };
